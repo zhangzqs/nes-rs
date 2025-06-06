0f221f5c5213b7eed65c865892a38071ff7e6c06
zzq <i@zhangzqs.cn>
2025-06-06 09:27:24 +0800

    CPU简单指令测试

--- nes-test/src/lib.rs
+++ nes-test/src/lib.rs
@@ -141,4 +141,50 @@ mod tests {
             }
         }
     }
+
+    #[test]
+    fn test_cpu_simple_instructions() {
+        let mut board = new_board();
+        board.reset();
+
+        let program: &[u8] = &[
+            0xa9, 0x10, // LDA #$10     -> A = #$10
+            0x85, 0x20, // STA $20      -> $20 = #$10
+            0xa9, 0x01, // LDA #$1      -> A = #$1
+            0x65, 0x20, // ADC $20      -> A = #$11
+            0x85, 0x21, // STA $21      -> $21=#$11
+            0xe6, 0x21, // INC $21      -> $21=#$12
+            0xa4, 0x21, // LDY $21      -> Y=#$12
+            0xc8, // INY          -> Y=#$13
+            0x00, // BRK
+        ];
+        for (i, &byte) in program.iter().enumerate() {
+            board.cpu_bus.borrow_mut().write(0xc000 + i as u16, byte);
+        }
+        board.cpu.borrow_mut().set_reg_pc(0xc000);
+        let run_once = || {
+            loop {
+                board.cpu.borrow_mut().clock();
+                if board.cpu.borrow().dump_state().remaining_cycles == 0 {
+                    break;
+                }
+            }
+        };
+        run_once();
+        assert_eq!(board.cpu.borrow().dump_state().reg_a, 0x10);
+        run_once();
+        assert_eq!(board.cpu_bus.borrow().read(0x20), 0x10);
+        run_once();
+        assert_eq!(board.cpu.borrow().dump_state().reg_a, 0x01);
+        run_once();
+        assert_eq!(board.cpu.borrow().dump_state().reg_a, 0x11);
+        run_once();
+        assert_eq!(board.cpu_bus.borrow().read(0x21), 0x11);
+        run_once();
+        assert_eq!(board.cpu_bus.borrow().read(0x21), 0x12);
+        run_once();
+        assert_eq!(board.cpu.borrow().dump_state().reg_y, 0x12);
+        run_once();
+        assert_eq!(board.cpu.borrow().dump_state().reg_y, 0x13);
+    }
 }
